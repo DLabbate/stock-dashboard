c9cc1db02383f4fbd8a5d54dd0d8002f2ea04a19
Domenic Labbate <domeniclabbate@gmail.com>
2021-12-24 04:32:16 -0500

    styling

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -4,15 +4,18 @@ import Card from "./Card";
 const Overview = ({ symbol, price, change, changePercent }) => {
   return (
     <Card>
-      <span className="absolute left-4 top-4 text-neutral-400 text-xl">
+      <span className="absolute left-4 top-4 text-neutral-400 text-xl 2xl:text-2xl">
         {symbol}
       </span>
       <div className="w-full h-full flex items-center justify-around">
-        <span className="text-4xl">
-          ${price} <span className="text-lg text-neutral-400">(USD)</span>
+        <span className="text-4xl 2xl:text-5xl flex items-center">
+          ${price}{" "}
+          <span className="text-xl 2xl:text-2xl text-neutral-400 m-2">
+            (USD)
+          </span>
         </span>
 
-        <span className="text-xl text-lime-500">
+        <span className="text-xl 2xl:text-2xl text-lime-500">
           +{change} <span>(+{changePercent}%)</span>
         </span>
       </div>
--- src/components/Search.js
+++ src/components/Search.js
@@ -48,9 +48,9 @@ const Search = () => {
         stroke="#737373"
       >
         <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
           d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
         />
       </svg>

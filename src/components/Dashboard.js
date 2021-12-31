3ae3195516347d61d2e490f32b9e555b1fb1d177
Domenic Labbate <domeniclabbate@gmail.com>
2021-12-31 02:06:05 -0500

    made dashboard more responsive

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -10,11 +10,11 @@ const Dashboard = () => {
   const { darkMode } = useContext(ThemeContext);
   return (
     <div
-      className={`h-screen grid grid-cols-4 xl:grid-cols-3 grid-rows-7 xl:grid-rows-5 auto-rows-fr gap-2 bg-neutral-100 p-8 font-raleway ${
+      className={`h-screen grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 grid-rows-7 xl:grid-rows-5 auto-rows-fr gap-2 bg-neutral-100 p-8 font-raleway ${
         darkMode ? "bg-gray-900 text-gray-300" : null
       }`}
     >
-      <div className="col-span-4 xl:col-span-3 row-span-1 flex justify-start items-center">
+      <div className="col-span-1 md:col-span-2 xl:col-span-3 row-span-1 flex justify-start items-center">
         <div className="xl:px-32">
           <h1 className="text-5xl">Apple Inc.</h1>
           <Search />
@@ -22,10 +22,10 @@ const Dashboard = () => {
 
         <ThemeIcon />
       </div>
-      <div className="col-span-4 xl:col-span-2 row-span-4 p-2">
+      <div className="md:col-span-2 row-span-4 p-2">
         <Chart />
       </div>
-      <div className="col-span-2 xl:col-span-1 p-2">
+      <div className="p-2">
         <Overview
           symbol={"AAPL"}
           price={130.63}
@@ -33,7 +33,7 @@ const Dashboard = () => {
           changePercent={0.67}
         />
       </div>
-      <div className="col-span-2 xl:col-span-1 row-span-2 xl:row-span-3 p-2">
+      <div className="row-span-2 xl:row-span-3 p-2">
         <Details />
       </div>
     </div>

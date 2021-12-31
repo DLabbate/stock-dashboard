988df935609acabf354312aa769e4c86982bb1ef
Domenic Labbate <domeniclabbate@gmail.com>
2021-12-31 01:37:33 -0500

    created dashboard component

--- src/App.js
+++ src/App.js
@@ -1,44 +1,13 @@
 import { useState } from "react";
 import "./App.css";
-import Chart from "./components/Chart";
-import Details from "./components/Details";
-import Overview from "./components/Overview";
-import Search from "./components/Search";
-import ThemeIcon from "./components/ThemeIcon";
+import Dashboard from "./components/Dashboard";
 import ThemeContext from "./context/ThemeContext";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
   return (
     <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
-      <div
-        className={`h-screen grid grid-cols-4 xl:grid-cols-3 grid-rows-7 xl:grid-rows-5 auto-rows-fr gap-2 bg-neutral-100 p-8 font-raleway ${
-          darkMode ? "bg-gray-900 text-gray-300" : null
-        }`}
-      >
-        <div className="col-span-4 xl:col-span-3 row-span-1 flex justify-start items-center">
-          <div className="xl:px-32">
-            <h1 className="text-5xl">Apple Inc.</h1>
-            <Search />
-          </div>
-
-          <ThemeIcon />
-        </div>
-        <div className="col-span-4 xl:col-span-2 row-span-4 p-2">
-          <Chart />
-        </div>
-        <div className="col-span-2 xl:col-span-1 p-2">
-          <Overview
-            symbol={"AAPL"}
-            price={130.63}
-            change={3.63}
-            changePercent={0.67}
-          />
-        </div>
-        <div className="col-span-2 xl:col-span-1 row-span-2 xl:row-span-3 p-2">
-          <Details />
-        </div>
-      </div>
+      <Dashboard />
     </ThemeContext.Provider>
   );
 }

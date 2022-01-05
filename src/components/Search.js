53d1604d36e267f85e80632fb317c3aaf8d8f043
Domenic Labbate <domeniclabbate@gmail.com>
2022-01-04 21:15:36 -0500

    refactored search functionality (onClick)

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,49 +1,29 @@
-import React, { useContext, useEffect, useState } from "react";
-import { sampleSearchOptions } from "../constants/mock";
-import StockContext from "../context/StockContext";
+import React, { useContext, useState } from "react";
 import ThemeContext from "../context/ThemeContext";
 import { searchSymbol } from "../utils/api/stock-api";
+import SearchResults from "./SearchResults";
 
 const Search = () => {
   const { darkMode } = useContext(ThemeContext);
-  const { setStockSymbol } = useContext(StockContext);
   const [input, setInput] = useState("");
   const [bestMatches, setBestMatches] = useState([]);
 
-  useEffect(() => {
-    const updateBestMatches = async () => {
-      const searchResults = await searchSymbol(input);
-      const result = searchResults.result;
-      if (result && result.length > 0) {
-        setBestMatches(result);
-      } else {
-        setBestMatches([]);
-      }
-    };
-    if (input) {
-      updateBestMatches();
+  const updateBestMatches = async () => {
+    const searchResults = await searchSymbol(input);
+    const result = searchResults.result;
+    if (result) {
+      setBestMatches(result.splice(0, 10));
+    } else {
+      setBestMatches([]);
     }
-  }, [input]);
+  };
+
   return (
     <div
       className={`flex items-center my-4 border-2 rounded-md relative z-50 ${
         darkMode ? "bg-gray-900 border-gray-800" : "bg-white border-neutral-200"
       }`}
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="h-6 w-6 m-2"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="#737373"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={2}
-          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-        />
-      </svg>
       <input
         type="text"
         value={input}
@@ -52,31 +32,29 @@ const Search = () => {
         }`}
         placeholder="Search stock..."
         onChange={(event) => setInput(event.target.value)}
+        onKeyPress={(event) => {
+          if (event.key === "Enter") {
+            updateBestMatches();
+          }
+        }}
       />
-      {
-        <ul
-          className={`absolute top-12 border-2 w-full rounded-md h-64 overflow-y-scroll custom-scrollbar ${
-            darkMode
-              ? "bg-gray-900 border-gray-800"
-              : "bg-white border-neutral-200 "
-          }`}
+      <button onClick={updateBestMatches}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6 m-2"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="#737373"
         >
-          {bestMatches.map((item) => {
-            return (
-              <li
-                key={item.symbol}
-                className={`cursor-pointer p-4 m-2 flex items-center justify-between rounded-md ${
-                  darkMode ? "hover:bg-indigo-600" : "hover:bg-indigo-200 "
-                }`}
-                onClick={() => setStockSymbol(item.symbol)}
-              >
-                <span>{item.symbol}</span>
-                <span>{item.description}</span>
-              </li>
-            );
-          })}
-        </ul>
-      }
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+          />
+        </svg>
+      </button>
+      {bestMatches.length > 0 ? <SearchResults results={bestMatches} /> : null}
     </div>
   );
 };

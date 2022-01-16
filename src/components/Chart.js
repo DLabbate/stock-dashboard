18a524dee18494fd867b67bf4f3972ad108c3d4c
Domenic Labbate <domeniclabbate@gmail.com>
2022-01-15 21:23:41 -0500

    set default timeline to 1W

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -20,7 +20,7 @@ import {
 import { chartConfig } from "../constants/config";
 
 const Chart = () => {
-  const [filter, setFilter] = useState("1D");
+  const [filter, setFilter] = useState("1W");
 
   const { darkMode } = useContext(ThemeContext);
 

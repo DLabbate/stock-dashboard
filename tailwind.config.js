3547ea1ef01c63a1b7132094c2369df3528c5938
Domenic Labbate <domeniclabbate@gmail.com>
2022-01-20 18:08:18 -0500

    google font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: "white",
+        none: "none",
+      },
+      borderWidth: {
+        1: "1px",
+      },
+      fontFamily: {
+        quicksand: ["Quicksand", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };

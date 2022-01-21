4484a39381e113aa159fb0d1ba8e845b75bff2ee
Domenic Labbate <domeniclabbate@gmail.com>
2022-01-21 00:27:05 -0500

    chart

---
+++ src/components/Chart.js
@@ -0,0 +1,74 @@
+import React from "react";
+import { useState } from "react/cjs/react.development";
+import { mockHistoricalData } from "../constants/mock";
+import { convertUnixTimestampToDate } from "../utils/helpers/date-helper";
+import {
+  Area,
+  XAxis,
+  YAxis,
+  ResponsiveContainer,
+  AreaChart,
+  Tooltip,
+} from "recharts";
+import Card from "./Card";
+import { chartConfig } from "../constants/config";
+import ChartFilter from "./ChartFilter";
+
+const Chart = () => {
+  const [data, setData] = useState(mockHistoricalData);
+  const [filter, setFilter] = useState("1W");
+
+  const formatData = (data) => {
+    return data.c.map((item, index) => {
+      return {
+        value: item.toFixed(2),
+        date: convertUnixTimestampToDate(data.t[index]),
+      };
+    });
+  };
+
+  return (
+    <Card>
+      <ul className="flex absolute top-2 right-2 z-40">
+        {Object.keys(chartConfig).map((item) => (
+          <li key={item}>
+            <ChartFilter
+              text={item}
+              active={filter === item}
+              onClick={() => {
+                setFilter(item);
+              }}
+            />
+          </li>
+        ))}
+      </ul>
+      <ResponsiveContainer>
+        <AreaChart data={formatData(data)}>
+          <defs>
+            <linearGradient id="chartColor" x1="0" y1="0" x2="0" y2="1">
+              <stop
+                offset="5%"
+                stopColor="rgb(199 210 254)"
+                stopOpacity={0.8}
+              />
+              <stop offset="95%" stopColor="rgb(199 210 254)" stopOpacity={0} />
+            </linearGradient>
+          </defs>
+          <Area
+            type="monotone"
+            dataKey="value"
+            stroke="#312e81"
+            fillOpacity={1}
+            fill="url(#chartColor)"
+            strokeWidth={0.5}
+          />
+          <Tooltip />
+          <XAxis dataKey="date" />
+          <YAxis domain={["dataMin", "dataMax"]} />
+        </AreaChart>
+      </ResponsiveContainer>
+    </Card>
+  );
+};
+
+export default Chart;
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,9 @@
 import React from "react";
-import Card from "./Card";
 import Header from "./Header";
 import { mockCompanyDetails, mockStockQuote } from "../constants/mock";
 import Details from "./Details";
 import Overview from "./Overview";
+import Chart from "./Chart";
 
 const Dashboard = () => {
   return (
@@ -12,7 +12,7 @@ const Dashboard = () => {
         <Header name={mockCompanyDetails.name} />
       </div>
       <div className="md:col-span-2 row-span-4">
-        <Card>Chart</Card>
+        <Chart />
       </div>
       <div>
         <Overview

f629f5985cbf94761794901a027e2afb71aff5e6
Domenic Labbate <domeniclabbate@gmail.com>
2022-01-20 20:36:58 -0500

    styling

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,17 +3,17 @@ import Card from "./Card";
 
 const Dashboard = () => {
   return (
-    <div className="h-screen grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 grid-rows-8 xl:grid-rows-5 auto-rows-fr gap-2 p-8 font-quicksand">
-      <div className="col-span-1 md:col-span-2 xl:col-span-3 row-span-1 p-2">
+    <div className="h-screen grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 grid-rows-8 xl:grid-rows-5 auto-rows-fr gap-6 p-10 font-quicksand">
+      <div className="col-span-1 md:col-span-2 xl:col-span-3 row-span-1">
         <Card>Header</Card>
       </div>
-      <div className="md:col-span-2 row-span-4 p-2">
+      <div className="md:col-span-2 row-span-4">
         <Card>Chart</Card>
       </div>
-      <div className="p-2">
+      <div>
         <Card>Overview</Card>
       </div>
-      <div className="row-span-2 xl:row-span-3 p-2">
+      <div className="row-span-2 xl:row-span-3">
         <Card>Details</Card>
       </div>
     </div>

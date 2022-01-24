9db925b27c5e6a1521ef2fdfec3704d58f9bba0f
Domenic Labbate <domeniclabbate@gmail.com>
2022-01-24 16:18:38 -0500

    theme

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -7,7 +7,7 @@ import Chart from "./Chart";
 
 const Dashboard = () => {
   return (
-    <div className="h-screen grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 grid-rows-8 xl:grid-rows-5 auto-rows-fr gap-6 p-10 font-quicksand bg-neutral-100">
+    <div className="h-screen grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 grid-rows-8 md:grid-rows-7 xl:grid-rows-5 auto-rows-fr gap-6 p-10 font-quicksand bg-neutral-100">
       <div className="col-span-1 md:col-span-2 xl:col-span-3 row-span-1 flex justify-start items-center">
         <Header name={mockCompanyDetails.name} />
       </div>

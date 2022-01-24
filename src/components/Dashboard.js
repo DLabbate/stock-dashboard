a2803230f26b91e618ba99393d60aaab9cdec530
Domenic Labbate <domeniclabbate@gmail.com>
2022-01-24 16:16:26 -0500

    theme extend

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,7 +3,7 @@ import Card from "./Card";
 
 const Dashboard = () => {
   return (
-    <div className="h-screen grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 grid-rows-8 xl:grid-rows-5 auto-rows-fr gap-6 p-10 font-quicksand">
+    <div className="h-screen grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 grid-rows-8 md:grid-rows-7 xl:grid-rows-5 auto-rows-fr gap-6 p-10 font-quicksand">
       <div className="col-span-1 md:col-span-2 xl:col-span-3 row-span-1">
         <Card>Header</Card>
       </div>
